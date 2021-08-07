234d7b387849cc7ad99e3ed5b02e9dad6e0c8f0a
Dennis Hezel <dh@3yourmind.com>
2021-08-07 18:22:00 +0200

    chore: Adjust working directory of vcpkg step

--- tests/utils/freePort.hpp
+++ tests/utils/freePort.hpp
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef AGRPC_UTILS_FREEPORT_HPP
+#define AGRPC_UTILS_FREEPORT_HPP
 
 #include <cstdint>
 
@@ -6,3 +7,5 @@ namespace agrpc::test
 {
 uint16_t get_free_port();
 }
+
+#endif  // AGRPC_UTILS_FREEPORT_HPP
