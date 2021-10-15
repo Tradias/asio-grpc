064acca7918b99228d74c64c124c7eb11a0fd6db
Dennis Hezel <dh@3yourmind.com>
2021-10-15 20:23:08 +0200

    docs: Add signal_set to helloworld server example to show correct shutdown of grpc::Server

--- example/hello-world-server-cpp20.cpp
+++ example/hello-world-server-cpp20.cpp
@@ -17,16 +17,23 @@
 #include <agrpc/asioGrpc.hpp>
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/detached.hpp>
+#include <boost/asio/signal_set.hpp>
 #include <grpcpp/server.h>
 #include <grpcpp/server_builder.h>
 
+#include <optional>
+#include <thread>
+
 int main()
 {
+    std::optional<std::thread> shutdown_thread;
+
     // begin-snippet: server-side-helloworld
     grpc::ServerBuilder builder;
     std::unique_ptr<grpc::Server> server;
     helloworld::Greeter::AsyncService service;
     agrpc::GrpcContext grpc_context{builder.AddCompletionQueue()};
+    boost::asio::basic_signal_set signals{grpc_context, SIGINT, SIGTERM};
     builder.AddListeningPort("0.0.0.0:50051", grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
     server = builder.BuildAndStart();
@@ -35,18 +42,41 @@ int main()
         grpc_context,
         [&]() -> boost::asio::awaitable<void>
         {
-            grpc::ServerContext server_context;
-            helloworld::HelloRequest request;
-            grpc::ServerAsyncResponseWriter<helloworld::HelloReply> writer{&server_context};
-            bool request_ok = co_await agrpc::request(&helloworld::Greeter::AsyncService::RequestSayHello, service,
-                                                      server_context, request, writer);
-            helloworld::HelloReply response;
-            response.set_message("Hello " + request.name());
-            bool finish_ok = co_await agrpc::finish(writer, response, grpc::Status::OK);
+            while (true)
+            {
+                grpc::ServerContext server_context;
+                helloworld::HelloRequest request;
+                grpc::ServerAsyncResponseWriter<helloworld::HelloReply> writer{&server_context};
+                bool request_ok = co_await agrpc::request(&helloworld::Greeter::AsyncService::RequestSayHello, service,
+                                                          server_context, request, writer);
+                if (!request_ok)
+                {
+                    co_return;
+                }
+                helloworld::HelloReply response;
+                response.set_message("Hello " + request.name());
+                bool finish_ok = co_await agrpc::finish(writer, response, grpc::Status::OK);
+            }
         },
         boost::asio::detached);
+    // end-snippet
+
+    signals.async_wait(
+        [&](auto&&, auto&&)
+        {
+            // This will cause all coroutines to run to completion normally
+            // while returning 'false' from RPC related steps
+            shutdown_thread.emplace(
+                [&]
+                {
+                    server->Shutdown();
+                });
+
+            // Or call 'grpc_context.stop()' here instead which causes all coroutines
+            // to end at their next suspension point.
+            // Then call 'server->Shutdown()' after the call to 'grpc_context.run()' returns.
+        });
 
     grpc_context.run();
-    server->Shutdown();
-    // end-snippet
+    shutdown_thread->join();
 }
