5d6fb5b74d4443e9276d81fa6f39335e354da266
Dennis Hezel <dh@3yourmind.com>
2021-11-12 20:11:20 +0100

    chore: More clang build debugging

--- .github/vcpkg/linux-clang.cmake
+++ .github/vcpkg/linux-clang.cmake
@@ -1,58 +1,17 @@
-if(NOT _VCPKG_LINUX_TOOLCHAIN)
-    set(_VCPKG_LINUX_TOOLCHAIN 1)
+if(NOT _VCPKG_LINUX_CLANG_TOOLCHAIN)
+    set(_VCPKG_LINUX_CLANG_TOOLCHAIN 1)
     set(CMAKE_C_COMPILER "clang")
     set(CMAKE_CXX_COMPILER "clang++")
 
-    if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
-        set(CMAKE_CROSSCOMPILING
-            OFF
-            CACHE BOOL "")
-    endif()
+    set(CMAKE_CROSSCOMPILING
+        OFF
+        CACHE BOOL "")
     set(CMAKE_SYSTEM_NAME
         Linux
         CACHE STRING "")
-    if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-        set(CMAKE_SYSTEM_PROCESSOR
-            x86_64
-            CACHE STRING "")
-    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-        set(CMAKE_SYSTEM_PROCESSOR
-            x86
-            CACHE STRING "")
-        string(APPEND VCPKG_C_FLAGS " -m32")
-        string(APPEND VCPKG_CXX_FLAGS " -m32")
-    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
-        set(CMAKE_SYSTEM_PROCESSOR
-            armv7l
-            CACHE STRING "")
-        if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux" AND CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64")
-            if(NOT DEFINED CMAKE_CXX_COMPILER)
-                set(CMAKE_CXX_COMPILER "arm-linux-gnueabihf-g++")
-            endif()
-            if(NOT DEFINED CMAKE_C_COMPILER)
-                set(CMAKE_C_COMPILER "arm-linux-gnueabihf-gcc")
-            endif()
-            message(
-                STATUS
-                    "Cross compiling arm on host x86_64, use cross compiler: ${CMAKE_CXX_COMPILER}/${CMAKE_C_COMPILER}")
-        endif()
-    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
-        set(CMAKE_SYSTEM_PROCESSOR
-            aarch64
-            CACHE STRING "")
-        if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux" AND CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64")
-            if(NOT DEFINED CMAKE_CXX_COMPILER)
-                set(CMAKE_CXX_COMPILER "aarch64-linux-gnu-g++")
-            endif()
-            if(NOT DEFINED CMAKE_C_COMPILER)
-                set(CMAKE_C_COMPILER "aarch64-linux-gnu-gcc")
-            endif()
-            message(
-                STATUS
-                    "Cross compiling arm64 on host x86_64, use cross compiler: ${CMAKE_CXX_COMPILER}/${CMAKE_C_COMPILER}"
-            )
-        endif()
-    endif()
+    set(CMAKE_SYSTEM_PROCESSOR
+        x86_64
+        CACHE STRING "")
 
     get_property(_CMAKE_IN_TRY_COMPILE GLOBAL PROPERTY IN_TRY_COMPILE)
     if(NOT _CMAKE_IN_TRY_COMPILE)
