f179621e3ff5401b99e4c40ba2427a1a1ab7ffcf
Dennis Hezel <dh@3yourmind.com>
2022-01-28 11:11:57 +0100

    test: Change how arguments are captured into coroutine in CoSpawner to potentially work around AppleClang issue

--- example/helper/coSpawner.hpp
+++ example/helper/coSpawner.hpp
@@ -36,11 +36,10 @@ struct CoSpawner
         auto executor = request_context.get_executor();  // the executor of the CompletionHandler
         boost::asio::co_spawn(
             std::move(executor),
-            [captured_handler = handler,
-             request_context = std::move(request_context)]() mutable -> boost::asio::awaitable<void>
+            [](Handler handler, agrpc::RepeatedlyRequestContext<T> request_context) -> boost::asio::awaitable<void>
             {
-                co_await std::apply(std::move(captured_handler), request_context.args());
-            },
+                co_await std::apply(std::move(handler), request_context.args());
+            }(handler, std::move(request_context)),
             boost::asio::detached);
     }
 
