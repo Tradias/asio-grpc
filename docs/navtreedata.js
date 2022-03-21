e1486c2e65b47b5c6347caf120f3336fa4f1ab54
GitHub Action <action@github.com>
2022-03-21 16:24:07 +0000

    Re-generate Github Pages

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -58,7 +58,7 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "annotated.html",
-"structagrpc_1_1detail_1_1_request_fn.html#a1033698af3d6f1f47c3b94eef81f57cb"
+"structagrpc_1_1detail_1_1_request_fn.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
