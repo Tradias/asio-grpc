392f92f7cd3a085e80d422d0280a5f9b3a589139
GitHub Action <action@github.com>
2022-04-14 09:08:30 +0000

    Re-generate Github Pages

--- docs/search/functions_8.js
+++ docs/search/functions_8.js
@@ -3,5 +3,5 @@ var searchData=
   ['poll_0',['poll',['../classagrpc_1_1_grpc_context.html#a4eb77d4c7fe553bc7b8aa7bc0c73fc22',1,'agrpc::GrpcContext']]],
   ['pollcontext_1',['PollContext',['../classagrpc_1_1_poll_context.html#aeee8c9e0f75d6fbe29d582537051bb1d',1,'agrpc::PollContext']]],
   ['post_2',['post',['../classagrpc_1_1_basic_grpc_executor.html#aa55121002e69f6da4d193e2438556519',1,'agrpc::BasicGrpcExecutor']]],
-  ['prefer_3',['prefer',['../classagrpc_1_1_basic_grpc_executor.html#a64a880d5d9b37c55cacfa1a8ee0de254',1,'agrpc::BasicGrpcExecutor::prefer(asio::execution::relationship_t::fork_t) const noexcept -&gt; agrpc::BasicGrpcExecutor&lt; Allocator, detail::set_relationship_continuation(Options, false)&gt;'],['../classagrpc_1_1_basic_grpc_executor.html#abe981aab499deb8a8358a50433a16e01',1,'agrpc::BasicGrpcExecutor::prefer(asio::execution::relationship_t::continuation_t) const noexcept -&gt; agrpc::BasicGrpcExecutor&lt; Allocator, detail::set_relationship_continuation(Options, true)&gt;']]]
+  ['prefer_3',['prefer',['../classagrpc_1_1_basic_grpc_executor.html#ada9763f836d7e1af44a67ce09c8d6522',1,'agrpc::BasicGrpcExecutor::prefer(asio::execution::relationship_t::fork_t) const noexcept'],['../classagrpc_1_1_basic_grpc_executor.html#af81aa8e1195243f012aeab6f83653015',1,'agrpc::BasicGrpcExecutor::prefer(asio::execution::relationship_t::continuation_t) const noexcept']]]
 ];
