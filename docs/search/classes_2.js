3e3c51bf0be11088d6028eba5f573a483d98c856
GitHub Action <action@github.com>
2022-04-22 17:38:42 +0000

    Re-generate Github Pages

--- docs/search/classes_2.js
+++ docs/search/classes_2.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['cancelsafe_0',['CancelSafe',['../classagrpc_1_1_cancel_safe.html',1,'agrpc']]],
-  ['completiontoken_1',['CompletionToken',['../classagrpc_1_1_cancel_safe_1_1_completion_token.html',1,'agrpc::CancelSafe']]]
+  ['cancelsafe_0',['CancelSafe',['../classagrpc_1_1_cancel_safe.html',1,'agrpc']]]
 ];
