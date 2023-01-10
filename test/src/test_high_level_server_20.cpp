e5058a9679d0ab8bc795b03f9ea14cd61ca48587
Dennis Hezel <dh@3yourmind.com>
2023-01-10 12:18:16 +0100

    feat: Create a separate state that represents a server request and improve NotifyWhenDone performance

--- test/src/test_high_level_server_20.cpp
+++ test/src/test_high_level_server_20.cpp
@@ -34,12 +34,124 @@ AGRPC_NAMESPACE_BEGIN()
 
 namespace detail
 {
-template <auto PrepareAsync, class Executor>
+struct NotifyWhenDoneInitFunction
+{
+    grpc::ServerContext& server_context_;
+
+    void operator()(const agrpc::GrpcContext&, void* tag) const { server_context_.AsyncNotifyWhenDone(tag); }
+};
+}
+
+class NotifyWhenDone
+{
+  private:
+    template <class T>
+    struct Allocator;
+
+    struct CompletionHandler;
+
+    using Operation = detail::GrpcTagOperation<CompletionHandler>;
+
+    struct CompletionHandler
+    {
+        using allocator_type = Allocator<Operation>;
+
+        void operator()(bool) const
+        {
+            self_.running_ = false;
+            self_.safe_.token()();
+        }
+
+        allocator_type get_allocator() const noexcept;
+
+        NotifyWhenDone& self_;
+    };
+
+    static_assert(std::is_trivially_destructible_v<Operation>);
+
+    template <class Handler>
+    void async_notify_when_done(agrpc::GrpcContext& grpc_context, grpc::ServerContext& server_context,
+                                Handler&& handler)
+    {
+        detail::NotifyWhenDoneInitFunction init{server_context};
+        detail::grpc_submit(grpc_context, init, std::forward<Handler>(handler));
+    }
+
+  public:
+    NotifyWhenDone() noexcept {}
+
+    void initiate(agrpc::GrpcContext& grpc_context, grpc::ServerContext& server_context)
+    {
+        async_notify_when_done(grpc_context, server_context, CompletionHandler{*this});
+    }
+
+    [[nodiscard]] bool is_running() const noexcept { return running_; }
+
+    template <class CompletionToken = agrpc::DefaultCompletionToken>
+    auto done(CompletionToken&& token = {})
+    {
+        return safe_.wait(static_cast<CompletionToken&&>(token));
+    }
+
+  private:
+    agrpc::CancelSafe<void()> safe_;
+    bool running_{true};
+    union
+    {
+        Operation operation_;
+    };
+};
+
+template <class T>
+struct NotifyWhenDone::Allocator
+{
+    using value_type = T;
+
+    Allocator() = default;
+
+    constexpr explicit Allocator(Operation* operation) noexcept : operation_(operation) {}
+
+    template <class U>
+    constexpr Allocator(const Allocator<U>& other) noexcept : operation_(other.operation_)
+    {
+    }
+
+    [[nodiscard]] constexpr T* allocate(std::size_t) noexcept { return operation_; }
+
+    static constexpr void deallocate(T*, std::size_t) noexcept {}
+
+    template <class U>
+    friend constexpr bool operator==(const Allocator&, const Allocator<U>&) noexcept
+    {
+        return true;
+    }
+
+    template <class U>
+    friend constexpr bool operator!=(const Allocator&, const Allocator<U>&) noexcept
+    {
+        return false;
+    }
+
+    template <class>
+    friend struct Allocator;
+
+    Operation* operation_;
+};
+
+inline NotifyWhenDone::CompletionHandler::allocator_type NotifyWhenDone::CompletionHandler::get_allocator()
+    const noexcept
+{
+    return Allocator<Operation>{&self_.operation_};
+}
+
+namespace detail
+{
+template <auto PrepareAsync, bool IsNotifyWhenDone>
 struct ServerServerStreamingRequestSenderImplementation;
 
 template <class Service, class Request, class Response, template <class> class Responder,
-          detail::ServerMultiArgRequest<Service, Request, Responder<Response>> PrepareAsync, class Executor>
-struct ServerServerStreamingRequestSenderImplementation<PrepareAsync, Executor>
+          detail::ServerMultiArgRequest<Service, Request, Responder<Response>> PrepareAsync>
+struct ServerServerStreamingRequestSenderImplementation<PrepareAsync, true>
 {
     static constexpr auto TYPE = detail::SenderImplementationType::GRPC_TAG;
 
@@ -48,7 +160,48 @@ struct ServerServerStreamingRequestSenderImplementation<PrepareAsync, Executor>
 
     struct Initiation
     {
-        agrpc::RPC<PrepareAsync, Executor>& rpc;
+        grpc::ServerContext& server_context;
+        Responder<Response>& responder;
+        Service& service;
+        Request& req;
+        agrpc::NotifyWhenDone& notify_when_done;
+    };
+
+    // detail::RPCClientContextBase& stop_function_arg(const Initiation&) noexcept { return rpc; }
+
+    void initiate(agrpc::GrpcContext& grpc_context, const Initiation& initiation, void* self) noexcept
+    {
+        auto& [server_context, responder, service, req, when_done] = initiation;
+        when_done.initiate(grpc_context, server_context);
+        grpc_context.work_finished();
+        (service.*PrepareAsync)(&server_context, &req, &responder, grpc_context.get_completion_queue(),
+                                grpc_context.get_server_completion_queue(), self);
+    }
+
+    template <class OnDone>
+    void done(OnDone on_done, bool ok)
+    {
+        if (ok)
+        {
+            on_done.grpc_context().work_started();
+        }
+        on_done(ok);
+    }
+};
+
+template <class Service, class Request, class Response, template <class> class Responder,
+          detail::ServerMultiArgRequest<Service, Request, Responder<Response>> PrepareAsync>
+struct ServerServerStreamingRequestSenderImplementation<PrepareAsync, false>
+{
+    static constexpr auto TYPE = detail::SenderImplementationType::GRPC_TAG;
+
+    using Signature = void(bool);
+    using StopFunction = detail::Empty;
+
+    struct Initiation
+    {
+        grpc::ServerContext& server_context;
+        Responder<Response>& responder;
         Service& service;
         Request& req;
     };
@@ -57,8 +210,8 @@ struct ServerServerStreamingRequestSenderImplementation<PrepareAsync, Executor>
 
     void initiate(agrpc::GrpcContext& grpc_context, const Initiation& initiation, void* self) noexcept
     {
-        auto& [rpc, service, req] = initiation;
-        (service.*PrepareAsync)(&rpc.server_context(), &req, &rpc.responder(), grpc_context.get_completion_queue(),
+        auto& [server_context, responder, service, req] = initiation;
+        (service.*PrepareAsync)(&server_context, &req, &responder, grpc_context.get_completion_queue(),
                                 grpc_context.get_server_completion_queue(), self);
     }
 
@@ -99,25 +252,17 @@ struct WriteServerStreamingSenderImplementation<Responder<Response>> : detail::G
 };
 }
 
-template <class ServiceT, class RequestT, class ResponseT, template <class> class ResponderT,
-          detail::ServerMultiArgRequest<ServiceT, RequestT, ResponderT<ResponseT>> PrepareAsync, class Executor>
-class RPC<PrepareAsync, Executor, agrpc::RPCType::SERVER_SERVER_STREAMING> : public detail::RPCExecutorBase<Executor>
+template <class Responder, class Executor = agrpc::GrpcExecutor>
+class State;
+
+template <class ResponseT, template <class> class ResponderT, class Executor>
+class State<ResponderT<ResponseT>, Executor> : public detail::RPCExecutorBase<Executor>
 {
-  public:
-    using Request = RequestT;
-    using Response = ResponseT;
+  private:
     using Responder = ResponderT<ResponseT>;
 
-    explicit RPC(const Executor& executor) : detail::RPCExecutorBase<Executor>{executor} {}
-
-    template <class CompletionToken = detail::DefaultCompletionTokenT<Executor>>
-    auto request(ServiceT& service, RequestT& request,
-                 CompletionToken token = detail::DefaultCompletionTokenT<Executor>{})
-    {
-        return detail::async_initiate_sender_implementation<
-            detail::ServerServerStreamingRequestSenderImplementation<PrepareAsync, Executor>>(
-            this->grpc_context(), {*this, service, request}, {}, token);
-    }
+  public:
+    explicit State(const Executor& executor) : detail::RPCExecutorBase<Executor>(executor) {}
 
     template <class CompletionToken = detail::DefaultCompletionTokenT<Executor>>
     auto write(const ResponseT& response, CompletionToken token = detail::DefaultCompletionTokenT<Executor>{})
@@ -131,50 +276,42 @@ class RPC<PrepareAsync, Executor, agrpc::RPCType::SERVER_SERVER_STREAMING> : pub
 
     grpc::ServerContext& server_context() noexcept { return server_context_; }
 
+    using detail::RPCExecutorBase<Executor>::grpc_context;
+
   private:
     grpc::ServerContext server_context_;
     Responder responder_{&server_context_};
 };
 
-class NotifyWhenDone
+template <class ServiceT, class RequestT, class ResponseT, template <class> class ResponderT,
+          detail::ServerMultiArgRequest<ServiceT, RequestT, ResponderT<ResponseT>> PrepareAsync, class Executor>
+class RPC<PrepareAsync, Executor, agrpc::RPCType::SERVER_SERVER_STREAMING> : public detail::RPCExecutorBase<Executor>
 {
-  private:
-    template <class CompletionToken>
-    auto async_notify_when_done(agrpc::GrpcContext& grpc_context, grpc::ServerContext& server_context,
-                                CompletionToken&& token)
-    {
-        return agrpc::grpc_initiate(
-            [&](const agrpc::GrpcContext&, void* tag)
-            {
-                server_context.AsyncNotifyWhenDone(tag);
-            },
-            asio::bind_executor(grpc_context, std::forward<CompletionToken>(token)));
-    }
-
   public:
-    void initiate(agrpc::GrpcContext& grpc_context, grpc::ServerContext& server_context)
+    using Request = RequestT;
+    using Response = ResponseT;
+    using Responder = ResponderT<ResponseT>;
+
+    explicit RPC(const Executor& executor) : detail::RPCExecutorBase<Executor>{executor} {}
+
+    template <class State, class CompletionToken = detail::DefaultCompletionTokenT<Executor>>
+    static auto request(State& state, ServiceT& service, RequestT& request,
+                        CompletionToken token = detail::DefaultCompletionTokenT<Executor>{})
     {
-        async_notify_when_done(grpc_context, server_context,
-                               buffer.bind_allocator(
-                                   [&](bool)
-                                   {
-                                       running = false;
-                                       safe.token()();
-                                   }));
+        return detail::async_initiate_sender_implementation<
+            detail::ServerServerStreamingRequestSenderImplementation<PrepareAsync, false>>(
+            state.grpc_context(), {state.server_context(), state.responder(), service, request}, {}, token);
     }
 
-    [[nodiscard]] bool is_running() const noexcept { return running; }
-
-    template <class CompletionToken = agrpc::DefaultCompletionToken>
-    auto done(CompletionToken&& token = {})
+    template <class State, class CompletionToken = detail::DefaultCompletionTokenT<Executor>>
+    static auto request(State& state, ServiceT& service, RequestT& request, agrpc::NotifyWhenDone& notify_when_done,
+                        CompletionToken token = detail::DefaultCompletionTokenT<Executor>{})
     {
-        return safe.wait(static_cast<CompletionToken&&>(token));
+        return detail::async_initiate_sender_implementation<
+            detail::ServerServerStreamingRequestSenderImplementation<PrepareAsync, true>>(
+            state.grpc_context(), {state.server_context(), state.responder(), service, request, notify_when_done}, {},
+            token);
     }
-
-  private:
-    agrpc::CancelSafe<void()> safe;
-    bool running{true};
-    example::Buffer<32> buffer;
 };
 
 AGRPC_NAMESPACE_END
@@ -183,17 +320,14 @@ template <auto PrepareAsync, class Service, class RequestHandler>
 asio::awaitable<void> request_loop(agrpc::GrpcContext& grpc_context, Service& service, RequestHandler request_handler)
 {
     using RPC = agrpc::RPC<PrepareAsync>;
-    RPC rpc{grpc_context.get_executor()};
+    agrpc::State<typename RPC::Responder> rpc{grpc_context.get_executor()};
     agrpc::NotifyWhenDone on_done;
-    on_done.initiate(grpc_context, rpc.server_context());
-    grpc_context.work_finished();
     typename RPC::Request request;
-    const bool ok = co_await rpc.request(service, request);
+    const bool ok = co_await RPC::request(rpc, service, request, on_done);
     if (!ok)
     {
         co_return;
     }
-    grpc_context.work_started();
     asio::co_spawn(grpc_context, request_loop<PrepareAsync>(grpc_context, service, request_handler), test::NoOp{});
     co_await request_handler(rpc, request, on_done);
     if (on_done.is_running())
@@ -211,7 +345,7 @@ TEST_CASE_FIXTURE(test::GrpcClientServerTest, "awaitableaa server streaming")
         {
             co_await request_loop<&test::v1::Test::AsyncService::RequestServerStreaming>(
                 grpc_context, service,
-                [&](auto& rpc, auto& request, auto&) -> asio::awaitable<void>
+                [&](auto& rpc, auto& request, auto& when_done) -> asio::awaitable<void>
                 {
                     CHECK_EQ(42, request.integer());
                     test::msg::Response response;
@@ -220,6 +354,7 @@ TEST_CASE_FIXTURE(test::GrpcClientServerTest, "awaitableaa server streaming")
                     CHECK(co_await rpc.write(response));
                     // CHECK(co_await rpc.finish(grpc::Status::OK));
                     shutdown.initiate();
+                    co_await when_done.done();
                     co_return;
                 });
         },
