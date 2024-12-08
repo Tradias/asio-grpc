f7f9fae3f9aa5cb62eae7c2ba0165d16b3d09886
GitHub Action <action@github.com>
2024-12-08 14:15:13 +0000

    Re-generate Github Pages

---
+++ docs/search/all_0.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['add_5fhealth_5fcheck_5fservice_0',['add_health_check_service',['../classagrpc_1_1_health_check_service.html#a886bd0e1412d8fc13ca2a5959aab07fb',1,'agrpc::HealthCheckService::add_health_check_service()'],['../classagrpc_1_1_health_check_service.html#a886bd0e1412d8fc13ca2a5959aab07fb',1,'agrpc::add_health_check_service()']]],
+  ['agrpc_1',['agrpc',['../namespaceagrpc.html',1,'']]],
+  ['alarm_2',['Alarm',['../namespaceagrpc.html#ad6c406cc03ead4c6f180bdf56bff6a03',1,'agrpc']]],
+  ['alarms_3',['Alarms',['../md_doc_2v3__migration.html#autotoc_md26',1,'']]],
+  ['allocator_4',['Custom allocator',['../md_doc_2completion__token.html#autotoc_md11',1,'']]],
+  ['allocator_5ftype_5',['allocator_type',['../classagrpc_1_1_grpc_context.html#a3445ceeb54e332eabf163cc6afdc1e3d',1,'agrpc::GrpcContext::allocator_type'],['../classagrpc_1_1_basic_grpc_executor.html#a658f9dcfd92c049d9b8961bbb67eab13',1,'agrpc::BasicGrpcExecutor::allocator_type']]],
+  ['as_5fdefault_5fon_5ft_6',['as_default_on_t',['../structagrpc_1_1_use_sender.html#a78ab5ead96e86416fd0ab76480d72139',1,'agrpc::UseSender']]],
+  ['asio_20grpc_7',['asio-grpc',['../index.html',1,'']]],
+  ['asio_20io_5fcontext_8',['Using Asio io_context',['../md_doc_2using__asio__io__context.html',1,'']]]
+];
---
+++ docs/search/functions_a.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['poll_0',['poll',['../classagrpc_1_1_grpc_context.html#a4eb77d4c7fe553bc7b8aa7bc0c73fc22',1,'agrpc::GrpcContext::poll()'],['../structagrpc_1_1_default_run_traits.html#a158e1228fe8b77b46fceb4a76d3d47f6',1,'agrpc::DefaultRunTraits::poll()']]],
+  ['poll_5fcompletion_5fqueue_1',['poll_completion_queue',['../classagrpc_1_1_grpc_context.html#af35c5d430df1d778320a2fe51ec40102',1,'agrpc::GrpcContext']]],
+  ['post_2',['post',['../classagrpc_1_1_basic_grpc_executor.html#aa55121002e69f6da4d193e2438556519',1,'agrpc::BasicGrpcExecutor']]],
+  ['prefer_3',['prefer',['../classagrpc_1_1_basic_grpc_executor.html#ada9763f836d7e1af44a67ce09c8d6522',1,'agrpc::BasicGrpcExecutor::prefer(asio::execution::relationship_t::fork_t) const noexcept'],['../classagrpc_1_1_basic_grpc_executor.html#af81aa8e1195243f012aeab6f83653015',1,'agrpc::BasicGrpcExecutor::prefer(asio::execution::relationship_t::continuation_t) const noexcept']]],
+  ['process_5fgrpc_5ftag_4',['process_grpc_tag',['../namespaceagrpc.html#accc81e3886098f4472dbe3e528594e3e',1,'agrpc']]]
+];
