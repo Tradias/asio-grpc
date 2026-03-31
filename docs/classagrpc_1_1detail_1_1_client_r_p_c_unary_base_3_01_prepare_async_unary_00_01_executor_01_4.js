4271f9db7b2f48ec2d25c6e8a5912efd49e0d7a0
GitHub Action <action@github.com>
2026-03-31 11:46:26 +0000

    Re-generate Github Pages

---
+++ docs/classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.js
@@ -0,0 +1,20 @@
+var classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4 =
+[
+    [ "rebind_executor", "structagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4_1_1rebind__executor.html", "structagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4_1_1rebind__executor" ],
+    [ "Stub", "classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.html#ae494858ba1fb77cc03a473d475f89fec", null ],
+    [ "Request", "classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.html#a3cbcb0c2a8dea17a5f8e2031b5f0e135", null ],
+    [ "Response", "classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.html#a42c626f687ccf2ac90c4f97c1428ba05", null ],
+    [ "executor_type", "classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.html#a4bb04cc6b06744978476e7566c776c5c", null ],
+    [ "service_name", "classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.html#a0d95b3e778fd04576727a180ced2ad49", null ],
+    [ "method_name", "classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.html#adca43de2480909300ee83f4e7d0fefe8", null ],
+    [ "request", "classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.html#a53680b344c2176c7ddd5fd97e331517a", null ],
+    [ "request", "classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.html#adc02ff48c51bd547e52fe0b69f671dc2", null ],
+    [ "start", "classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.html#a0cc8fb2b4fdbc30dfaa214b7faa16828", null ],
+    [ "finish", "classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.html#a630c84662c7ffef3f82799c92b32d313", null ],
+    [ "read_initial_metadata", "classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.html#a1f3342f57017775ec75f7f3317949b05", null ],
+    [ "get_executor", "classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.html#ac7d45c8fc3141d17881ac99ba0b53034", null ],
+    [ "get_scheduler", "classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.html#ac5e0d6c42c0ed62b84b90f3ce42e180a", null ],
+    [ "context", "classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.html#a056f94eadae189c43a6919d09e9f0031", null ],
+    [ "cancel", "classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.html#ade0c1cd5b69a524efbf92059fda88c1c", null ],
+    [ "TYPE", "classagrpc_1_1detail_1_1_client_r_p_c_unary_base_3_01_prepare_async_unary_00_01_executor_01_4.html#aebbb738d826d482f508a01cd59fcd6b4", null ]
+];
---
+++ docs/navtreedata.js
@@ -0,0 +1,107 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "asio-grpc", "index.html", [
+    [ "Overview", "index.html#autotoc_md13", null ],
+    [ "Client rpc cheat sheet", "md_doc_2client__rpc__cheat__sheet.html", [
+      [ "Unary rpc", "md_doc_2client__rpc__cheat__sheet.html#autotoc_md1", null ],
+      [ "Client-streaming rpc", "md_doc_2client__rpc__cheat__sheet.html#autotoc_md2", null ],
+      [ "Server-streaming rpc", "md_doc_2client__rpc__cheat__sheet.html#autotoc_md3", null ],
+      [ "Bidirectional-streaming rpc", "md_doc_2client__rpc__cheat__sheet.html#autotoc_md4", null ]
+    ] ],
+    [ "CMake protobuf generate", "md_doc_2cmake__protobuf__generate.html", null ],
+    [ "Completion token", "md_doc_2completion__token.html", [
+      [ "Callback", "md_doc_2completion__token.html#autotoc_md8", null ],
+      [ "Boost.Coroutine", "md_doc_2completion__token.html#autotoc_md9", null ],
+      [ "use_sender", "md_doc_2completion__token.html#autotoc_md10", null ],
+      [ "Custom allocator", "md_doc_2completion__token.html#autotoc_md11", null ]
+    ] ],
+    [ "Server rpc cheat sheet", "md_doc_2server__rpc__cheat__sheet.html", [
+      [ "Unary rpc", "md_doc_2server__rpc__cheat__sheet.html#autotoc_md15", null ],
+      [ "Client-streaming rpc", "md_doc_2server__rpc__cheat__sheet.html#autotoc_md16", null ],
+      [ "Server-streaming rpc", "md_doc_2server__rpc__cheat__sheet.html#autotoc_md17", null ],
+      [ "Bidirectional-streaming rpc", "md_doc_2server__rpc__cheat__sheet.html#autotoc_md18", null ]
+    ] ],
+    [ "Using Asio io_context", "md_doc_2using__asio__io__context.html", [
+      [ "Implicitly constructed io_context", "md_doc_2using__asio__io__context.html#autotoc_md20", null ],
+      [ "Explicitly constructed io_context", "md_doc_2using__asio__io__context.html#autotoc_md21", [
+        [ "Conclusion", "md_doc_2using__asio__io__context.html#autotoc_md22", null ]
+      ] ],
+      [ "Why not use io_context for gRPC directly?", "md_doc_2using__asio__io__context.html#autotoc_md23", null ]
+    ] ],
+    [ "V3 migration guide", "md_doc_2v3__migration.html", [
+      [ "CMake", "md_doc_2v3__migration.html#autotoc_md25", null ],
+      [ "Alarms", "md_doc_2v3__migration.html#autotoc_md26", null ],
+      [ "Client RPCs", "md_doc_2v3__migration.html#autotoc_md27", [
+        [ "Unary", "md_doc_2v3__migration.html#autotoc_md28", null ],
+        [ "Client-streaming", "md_doc_2v3__migration.html#autotoc_md29", null ],
+        [ "Server-streaming", "md_doc_2v3__migration.html#autotoc_md30", null ],
+        [ "Bidirectional-streaming", "md_doc_2v3__migration.html#autotoc_md31", null ]
+      ] ],
+      [ "Server RPCs", "md_doc_2v3__migration.html#autotoc_md32", [
+        [ "Unary", "md_doc_2v3__migration.html#autotoc_md33", null ],
+        [ "Client-streaming", "md_doc_2v3__migration.html#autotoc_md34", null ],
+        [ "Server-streaming", "md_doc_2v3__migration.html#autotoc_md35", null ],
+        [ "Bidirectional-streaming", "md_doc_2v3__migration.html#autotoc_md36", null ]
+      ] ]
+    ] ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classagrpc_1_1_client_r_p_c_3_01_prepare_async_bidi_streaming_00_01_executor_01_4.html#a4ad1384caca1951c4d7b4b68b1387bb9",
+"classagrpc_1_1_server_r_p_c_3_01_request_bidi_streaming_00_01_traits_t_00_01_executor_01_4.html#a42255cc87cf854a4d2bc04315f7647d5",
+"classagrpc_1_1detail_1_1_client_r_p_c_base.html#ac5e0d6c42c0ed62b84b90f3ce42e180a",
+"structagrpc_1_1_basic_client_write_reactor_1_1rebind__executor.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronization';
+var SYNCOFFMSG = 'click to enable panel synchronization';
+var LISTOFALLMEMBERS = 'List of all members';
